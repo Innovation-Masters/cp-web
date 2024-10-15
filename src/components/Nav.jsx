4e5fa01b5aca97a83abe64d371e75be657462e92
Leonardo <leonardorscarpitta@gmail.com>
2024-10-15 14:35:04 -0300

    style: set header to position sticky

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 export default function Nav() {
     return (
-        <header className="flex justify-evenly items-center py-8">
-            <h1 className="text-2xl font-bold text-secondary">Innova Jewelry</h1>
+        <header className="flex justify-evenly items-center py-8 sticky top-0 bg-dark">
+            <h1 className="text-2xl font-extrabold text-light">Innova Jewelry</h1>
             <nav>
-                <ul className="flex text-dark font-bold gap-x-8 items-center text-lg">
+                <ul className="flex text-dark font-bold gap-x-8 items-center text-lg text-light">
                     <a className="hover:opacity-70" href="#">Home</a>
                     <a className="hover:opacity-70" href="#">Produtos</a>
                     <a className="hover:opacity-70" href="#">Sobre</a>
@@ -11,7 +11,7 @@ export default function Nav() {
                 </ul>
             </nav>
             <div className="flex gap-x-4 font-bold">
-                <button className="py-1 px-4 bg-dark rounded-lg hover:opacity-70 text-light">Acessar Conta</button>
+                <button className="py-1 px-4 bg-secondary rounded-lg hover:opacity-70 text-light">Acessar Conta</button>
                 <img className="hover:opacity-70 cursor-pointer" src="../../public/shop.png" alt="icone de carrinho de compras" width={32}/>
             </div>
         </header>
