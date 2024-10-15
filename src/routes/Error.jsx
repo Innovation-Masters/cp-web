06ec15f978a43110d7657774e19838492b19807c
Leonardo <leonardorscarpitta@gmail.com>
2024-10-15 17:19:12 -0300

    feat: create 404 page

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,7 +1,8 @@
 export default function Error() {
     return (
-        <>
-        
-        </>
+        <main className="bg-slate-900 min-h-screen flex flex-col gap-y-12 items-center">
+            <img src="../../public/error404.webp" width={400} />
+            <p className="font-extrabold text-3xl text-slate-50">Página não encontrada: <a href="#"><u>Clique aqui</u></a> para retornar ao menu!</p>
+        </main>
     )
 }
