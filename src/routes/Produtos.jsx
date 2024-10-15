8f25fdbf553b613eebb8d0ef38e1d297347655cc
Leonardo <leonardorscarpitta@gmail.com>
2024-10-15 17:08:02 -0300

    feat: update products page removin unused components

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,8 +1,6 @@
 export default function Produtos() {
     return (
         <main className="flex flex-col gap-y-12 mx-52 items-center">
-            <img className="rounded-3xl" src="../../public/banner.jpg" alt="banner da joalheria" width={1500} height={500} />
-            <h3 className="text-3xl uppercase font-bold self-start">Produtos em estoque</h3>
             <section className="grid grid-cols-3 gap-12">
                 <div className="flex flex-col gap-y-2">
                     <img src="../../public/colecao1.jpg" alt="imagem da coleção de joias" width={300} />
